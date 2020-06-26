b0025f1996a93e5c38262c368356a6d18fb94893
Elisabetta Manca <elisabetta.manca@cern.ch>
2020-06-26 17:36:32 +0200

    modules for defining SF from external file

--- analysisOnData/interface/weightDefinitions.hpp
+++ analysisOnData/interface/weightDefinitions.hpp
@@ -8,6 +8,7 @@
 #include "TH2D.h"
 #include "TString.h"
 #include "TMath.h"
+#include "TFile.h"
 #include "interface/module.hpp"
 #include "interface/functions.hpp"
 
@@ -20,14 +21,18 @@ private:
     std::vector<ROOT::RDF::RResultPtr<TH1D>> _h1List;
     std::vector<ROOT::RDF::RResultPtr<TH2D>> _h2List;
     std::vector<ROOT::RDF::RResultPtr<TH3D>> _h3List;
-
+    
     // groups of histos
     std::vector<ROOT::RDF::RResultPtr<std::vector<TH1D>>> _h1Group;
     std::vector<ROOT::RDF::RResultPtr<std::vector<TH2D>>> _h2Group;
     std::vector<ROOT::RDF::RResultPtr<std::vector<TH3D>>> _h3Group;
 
+    TFile *_SF;
+
 public:
-    ~weightDefinitions(){};
+    ~weightDefinitions(TFile *SF){
+        _SF = SF;
+    };
 
     RNode run(RNode) override;
     std::vector<ROOT::RDF::RResultPtr<TH1D>> getTH1() override;
