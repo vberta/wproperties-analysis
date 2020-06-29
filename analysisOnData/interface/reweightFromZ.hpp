d68956dc4221f1b3bb1044648ab3d032a1c47d0a
Elisabetta Manca <elisabetta.manca@cern.ch>
2020-06-29 22:32:40 +0200

    adding reweight module

---
+++ analysisOnData/interface/reweightFromZ.hpp
@@ -0,0 +1,67 @@
+#ifndef REWEIGHTFROMZ_H
+#define REWEIGHTFROMZ_H
+
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RVec.hxx"
+#include "ROOT/RDF/RInterface.hxx"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TString.h"
+#include "TMath.h"
+#include "TFile.h"
+#include "interface/module.hpp"
+#include "interface/functions.hpp"
+
+using RNode = ROOT::RDF::RNode;
+
+class reweightFromZ : public Module
+{
+
+private:
+    std::vector<ROOT::RDF::RResultPtr<TH1D>> _h1List;
+    std::vector<ROOT::RDF::RResultPtr<TH2D>> _h2List;
+    std::vector<ROOT::RDF::RResultPtr<TH3D>> _h3List;
+    
+    // groups of histos
+    std::vector<ROOT::RDF::RResultPtr<std::vector<TH1D>>> _h1Group;
+    std::vector<ROOT::RDF::RResultPtr<std::vector<TH2D>>> _h2Group;
+    std::vector<ROOT::RDF::RResultPtr<std::vector<TH3D>>> _h3Group;
+
+    TFile *_Pt;
+    TFile *_Y;
+
+    TH1F *_hPt;
+    TH1F *_hY;
+
+public:
+    reweightFromZ(TFile *Pt, TFile *Y)
+    {
+        _Pt = Pt;
+        _Y = Y;
+
+        TH1F *_hPt = (TH1F *)_Pt->Get("unfold");
+        TH1F *hPtMC = (TH1F *)_Pt->Get("hDDilPtLL");
+        TH1F *_hY = (TH1F *)_Y->Get("unfold");
+        TH1F *hYMC = (TH1F *)_Y->Get("hDDilRapLL");
+
+        _hPt->Divide(hPtMC);
+        _hPt->Scale(0.979);
+
+        hYMC->Scale(_hY->Integral() / hYMC->Integral());
+        _hY->Divide(hYMC);
+    };
+    ~reweightFromZ(){};
+
+        RNode run(RNode) override;
+    std::vector<ROOT::RDF::RResultPtr<TH1D>> getTH1() override;
+    std::vector<ROOT::RDF::RResultPtr<TH2D>> getTH2() override;
+    std::vector<ROOT::RDF::RResultPtr<TH3D>> getTH3() override;
+
+    std::vector<ROOT::RDF::RResultPtr<std::vector<TH1D>>> getGroupTH1() override;
+    std::vector<ROOT::RDF::RResultPtr<std::vector<TH2D>>> getGroupTH2() override;
+    std::vector<ROOT::RDF::RResultPtr<std::vector<TH3D>>> getGroupTH3() override;
+
+    void reset() override;
+};
+
+#endif
---
+++ analysisOnData/src/reweightFromZ.cpp
@@ -0,0 +1,68 @@
+#include "interface/reweightFromZ.hpp"
+
+RNode reweightFromZ::run(RNode d)
+{
+
+    auto getWeightPt = [this](float Pt) -> float {
+        int bin = _hPt->FindBin(Pt);
+        if (_hPt->IsBinOverflow(bin))
+            return _hPt->GetBinContent(_hPt->GetNbinsX());
+        else if (_hPt->IsBinUnderflow(bin))
+            return _hPt->GetBinContent(1);
+        else
+            return _hPt->GetBinContent(bin);
+    };
+    auto getWeightY = [this](float y) -> float {
+        float absy = TMath::Abs(y);
+        int bin = _hY->FindBin(absy);
+        if (_hY->IsBinOverflow(bin))
+            return _hY->GetBinContent(_hY->GetNbinsX());
+        else if (_hY->IsBinUnderflow(bin))
+            return _hY->GetBinContent(1);
+        else
+            return _hY->GetBinContent(bin);
+    };
+
+    auto d1 = d.Define("weightPt", getWeightPt, {"GenV_dress_qt"})
+               .Define("weightY", getWeightY, {"GenV_dress_y"});
+
+    return d1;
+}
+
+std::vector<ROOT::RDF::RResultPtr<TH1D>> reweightFromZ::getTH1()
+{
+    return _h1List;
+}
+std::vector<ROOT::RDF::RResultPtr<TH2D>> reweightFromZ::getTH2()
+{
+    return _h2List;
+}
+std::vector<ROOT::RDF::RResultPtr<TH3D>> reweightFromZ::getTH3()
+{
+    return _h3List;
+}
+
+std::vector<ROOT::RDF::RResultPtr<std::vector<TH1D>>> reweightFromZ::getGroupTH1()
+{
+    return _h1Group;
+}
+std::vector<ROOT::RDF::RResultPtr<std::vector<TH2D>>> reweightFromZ::getGroupTH2()
+{
+    return _h2Group;
+}
+std::vector<ROOT::RDF::RResultPtr<std::vector<TH3D>>> reweightFromZ::getGroupTH3()
+{
+    return _h3Group;
+}
+
+void reweightFromZ::reset()
+{
+
+    _h1List.clear();
+    _h2List.clear();
+    _h3List.clear();
+
+    _h1Group.clear();
+    _h2Group.clear();
+    _h3Group.clear();
+}
