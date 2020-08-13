ef005c77e0d5874f6cd2f0e81082ac87decbbd75
Suvankar Roy Chowdhury <sroychow@cern.ch>
2020-08-13 12:36:28 +0200

    pdf name correction

--- bkgAnalysis/bkg_utils.py
+++ bkgAnalysis/bkg_utils.py
@@ -14,7 +14,7 @@ bkg_systematics = {
     "LHEScaleWeightVars" : ["LHEScaleWeight_muR0p5_muF0p5", "LHEScaleWeight_muR0p5_muF1p0","LHEScaleWeight_muR1p0_muF0p5","LHEScaleWeight_muR1p0_muF2p0","LHEScaleWeight_muR2p0_muF1p0", "LHEScaleWeight_muR2p0_muF2p0"],
     "ptScaleVars" : [ "correctedUp", "correctedDown"], 
     "jmeVars" : [ "jerUp", "jerDown" , "jesTotalUp", "jesTotalDown", "unclustEnUp","unclustEnDown"],
-    "LHEPdfWeightVars" : ["_LHEPdfWeightHess{}".format(i+1) for i in range(60)]
+    "LHEPdfWeightVars" : ["LHEPdfWeightHess{}".format(i+1) for i in range(60)]
   }
 
 ptBinning = [25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55] 
