5623e798b32be1458706cf153a1e5a43aacf3fb7
emanca <elisabetta.manca@cern.ch>
2020-09-01 11:23:53 +0200

    revert wrong modification

--- analysisOnData/interface/baseDefinitions.hpp
+++ analysisOnData/interface/baseDefinitions.hpp
@@ -7,11 +7,16 @@ class baseDefinitions : public Module
 {
 
 private:
+    bool _isMC;
 
 public:
-
+    baseDefinitions(bool isMC = 1){
+      _isMC = isMC;
+    };
     ~baseDefinitions(){};
 
     RNode run(RNode) override;
+
 };
+
 #endif
