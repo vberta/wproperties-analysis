04d65553f6c99be6f927c252e02df500eee7859e
Elisabetta Manca <elisabetta.manca@cern.ch>
2020-10-08 18:09:20 +0200

    dict for nuisance almost complete. also added pre/postfit plots

--- Fit/systToapply.py
+++ Fit/systToapply.py
@@ -1,46 +1,58 @@
-##syst type and it's variations
 systematicsDict = {
-    "Nominal" : [""],
-    "mass" : ["mass"],
+    "Nominal": {
+        
+    },
+    "CMSlumi": {
+        vars:[],
+        procs: ["Signal", "DY","Diboson","Top","Tau","LowAcc"],
+        type: "logN"
+    },
+    "DYxsec":{
+        vars:[],
+        procs: ["DY"],
+        type: "logN"
+    },
+    "Topxsec":{
+        vars:[],
+        procs: ["Top"],
+        type: "logN"
+    },
+    "Dibosonxsec":{
+        vars:[],
+        procs: ["Diboson"],
+        type: "logN"
+    },
+    "mass" : {
+        vars:["mass"],
+        procs: ["Signal", "LowAcc"],
+        type:"shape"},
+    "WHSFVars"  : {
+        vars:["WHSFSyst0", "WHSFSyst1","WHSFSyst2","WHSFSystFlat"],
+        procs: ["Signal", "DY","Diboson","Top","Tau","LowAcc","Fakes"],
+        type:"shape"},
+    "ptScaleVars" : {
+        vars:["corrected"], 
+        procs: ["Signal", "DY","Diboson","Top","Tau","LowAcc","Fakes"],
+        type: "shape"
+    },
+    "jmeVars" : {
+        vars:["jesTotal", "unclustEn"],
+        procs: ["Signal", "DY","Diboson","Top","Tau","LowAcc","Fakes"],
+        type: "shape"
+    },
+    "PrefireWeight":{
+        vars:["PrefireWeight"],
+        procs: ["Signal", "DY","Diboson","Top","Tau","LowAcc","Fakes"],
+        type: "shape"
+    }
+    "LHEPdfWeightVars" : {
+        vars:["LHEPdfWeightHess{}".format(i+1) for i in range(60)],
+        procs: ["Signal", "DY","Tau","LowAcc"],
+        type: "shape"
+    },
+    "alphaS" :{
+        vars: ["alphaS"],
+        procs: ["Signal", "DY","Tau","LowAcc"],
+        type: "shape"
+    }
 }
-###Mapping of nuisance to which sample they are applicable
-systToSampleDict = {
-    "mass" : ["LowAcc"],
-}
-###mapping of nuisance and their type - 
-systTypeDict = {
-    "mass" : "shape",
-}
-
-'''
-#ideal
-systematicsDict = {
-    "Nominal" : [""],
-    "mass" : ["mass"],
-    "WHSFVars"  : ["WHSFSyst0", "WHSFSyst1","WHSFSyst2","WHSFSystFlat"],
-    "ptScaleVars" : [ "corrected"], 
-    "jmeVars" : ["jesTotal", "unclustEn"],
-    "LHEPdfWeightVars" : ["LHEPdfWeightHess{}".format(i+1) for i in range(60)]
-    #"LHEScaleWeightVars" : ["LHEScaleWeight_muR0p5_muF0p5", "LHEScaleWeight_muR0p5_muF1p0","LHEScaleWeight_muR1p0_muF0p5","LHEScaleWeight_muR1p0_muF2OAp0","LHEScaleWeight_muR2p0_muF1p0", "LHEScaleWeight_muR2p0_muF2p0"],
-}
-
-
-systToSampleDict = {
-    "mass" : ["Signal", "LowAcc"],
-    "WHSFVars"  : ["Signal", "DY","Diboson","Top","Tau","LowAcc"],                                                                        
-    "ptScaleVars" : ["Signal", "DY","Diboson","Top","Fake","Tau","LowAcc"],
-    "jmeVars" : ["Signal", "DY","Diboson","Top","Fake","Tau","LowAcc"],
-    "LHEPdfWeightVars" : ["Signal", "DY", "LowAcc", "Tau" ]                                                                  
-    #"LHEScaleWeightVars" : [] 
-}
-
-systType = {
-    "mass" : "shape"
-    "WHSFVars"  : "shape"
-    "ptScaleVars" : "shape"
-    "jmeVars" : "shape"
-    "LHEPdfWeightVars" : "shape" 
-    #"LHEScaleWeightVars" : [] 
-}
-
-'''
