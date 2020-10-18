3b0e1a58ac8ff21603978de3c2e402277c3acad9
Elisabetta Manca <elisabetta.manca@cern.ch>
2020-10-18 12:26:33 +0200

    updating plotter for wminus

--- analysisOnData/python/plotter_SignalACtemplates.py
+++ analysisOnData/python/plotter_SignalACtemplates.py
@@ -1,3 +1,4 @@
+from termcolor import colored
 from time import time
 import ROOT
 import copy
@@ -15,24 +16,31 @@ ROOT.TH2.AddDirectory(False)
 
 class plotter:
     
-    def __init__(self, outDir, ACfile, inDir = ''):
+    def __init__(self, outDir, inDir = ''):
         self.indir = inDir # indir containig the various outputs
         self.outdir = outDir
         self.sampleFile = 'WToMu_AC_plots.root'
 
         self.extSyst = copy.deepcopy(systematics)
         self.extSyst['Nominal'] =  (['', '_massUp', '_massDown'],"Nom")
-        self.histoDict ={} 
+        self.histoDict ={}
+        self.histoDict["Wplus"]={}
+        self.histoDict["Wminus"] = {}
         self.clist = ["L", "I", "T", "A", "P", "7", "8", "9", "UL"]
         self.varName = 'helXsecs'
         self.inFile = ROOT.TFile.Open(self.indir+'/'+self.sampleFile)
         if not self.inFile :
             print self.inFile, ' does not exist'
             sys.exit(1)
-        self.ACfile = ROOT.TFile.Open(ACfile)
-        self.imap = self.ACfile.Get("angularCoefficients/mapTot")
-        self.clos = copy.deepcopy(ROOT.TH2D("clos", "clos", self.imap.GetXaxis().GetNbins(),self.imap.GetXaxis().GetXbins().GetArray(),self.imap.GetYaxis().GetNbins(),self.imap.GetYaxis().GetXbins().GetArray()))
+        self.fileACplus = ROOT.TFile.Open("../../analysisOnGen/genInput_Wplus.root")
+        self.fileACminus = ROOT.TFile.Open("../../analysisOnGen/genInput_Wminus.root")
+        self.imapPlus = self.fileACplus.Get("angularCoefficients/mapTot")
+        self.imapMinus = self.fileACminus.Get("angularCoefficients/mapTot")
+        self.closPlus = copy.deepcopy(ROOT.TH2D("closPlus", "closPlus", self.imapPlus.GetXaxis().GetNbins(),self.imapPlus.GetXaxis().GetXbins().GetArray(),self.imapPlus.GetYaxis().GetNbins(),self.imapPlus.GetYaxis().GetXbins().GetArray()))
+        self.closMinus = copy.deepcopy(ROOT.TH2D("closMinus", "closMinus", self.imapPlus.GetXaxis().GetNbins(), self.imapPlus.GetXaxis().GetXbins().GetArray(), self.imapPlus.GetYaxis().GetNbins(), self.imapPlus.GetYaxis().GetXbins().GetArray()))
         self.yields = {}
+        self.yields["Wplus"] = {}
+        self.yields["Wminus"] = {}
         
         self.helXsecs = OrderedDict()
         self.helXsecs["L"] = "A0"
@@ -68,14 +76,23 @@ class plotter:
                 unrolledth2.SetBinContent(bin1D, th2.GetBinContent(ibin, jbin))
                 unrolledth2.SetBinError(bin1D, th2.GetBinError(ibin, jbin))
         return unrolledth2
-    def makeTH3slices(self, th3, systname, chargeBin):
+    def normaliseYields(self, th2, coeff, iY, iQt, imap, fAC):
+        #normalise templates to its helicity xsec
+        nsum = (3./16./math.pi)*imap.GetBinContent(iY, iQt)
+        if not 'UL' in th2.GetName():
+            hAC = fAC.Get("angularCoefficients/harmonics{}_nom_nom".format(self.helXsecs[coeff]))
+            nsum = nsum * hAC.GetBinContent(iY, iQt)/self.factors[self.helXsecs[coeff]]
+            th2.Scale(nsum)
+        return th2
+    def makeTH3slices(self, th3, systname):
         
         hname=th3.GetName()
-        iQt = int(hname.split('_')[1])
-        coeff = hname.split('_')[3]
+        charge = hname.split('_')[0]
+        iQt = int(hname.split('_')[2])
+        coeff = hname.split('_')[4]
         
         try:
-            syst = "_" + hname.split('_')[4]
+            syst = "_" + hname.split('_')[5]
         except IndexError:
             syst = ""
         
@@ -86,56 +103,58 @@ class plotter:
             
             th2slice=th3.Project3D("y_{iY}_yxe".format(iY=iY))
             th2slice.SetName(slicename)
-            #normalise templates to its helicity xsec
-            nsum = (3./16./math.pi)*self.imap.GetBinContent(iY,iQt)
-            if not 'UL' in hname:
-                hAC = self.ACfile.Get("angularCoefficients/harmonics{}_nom_nom".format(self.helXsecs[coeff]))
-                nsum = nsum*hAC.GetBinContent(iY,iQt)/self.factors[self.helXsecs[coeff]]
-            th2slice.Scale(nsum)
+            if charge == "Wplus":
+                self.normaliseYields(th2slice, coeff, iY, iQt, self.imapPlus, self.fileACplus)
+            else:
+                self.normaliseYields(th2slice, coeff, iY, iQt, self.imapMinus, self.fileACminus)
             th2slice.SetDirectory(0)
-            self.histoDict[systname].append(th2slice)
+            self.histoDict[charge][systname].append(th2slice)
 
             if syst=="":
                 #print th2slice.GetName(), th2slice.Integral(0,th2slice.GetNbinsX()+2,0,th2slice.GetNbinsY()+2)
-                self.yields[(iY,iQt)]+=th2slice.Integral(0,th2slice.GetNbinsX()+2,0,th2slice.GetNbinsY()+2)
+                self.yields[charge][(iY,iQt)]+=th2slice.Integral(0,th2slice.GetNbinsX()+2,0,th2slice.GetNbinsY()+2)
     def closureMap(self):
         for iY in range(1, 7):
             for iQt in range(1, 9):
-                self.clos.SetBinContent(iY,iQt, self.yields[(iY,iQt)])
-
-        self.clos.Divide(self.imap)
+                self.closPlus.SetBinContent(iY,iQt, self.yields["Wplus"][(iY,iQt)])
+                self.closMinus.SetBinContent(iY, iQt, self.yields["Wminus"][(iY, iQt)])
+        self.closPlus.Divide(self.imapPlus)
+        self.closMinus.Divide(self.imapMinus)
         for iY in range(1, 7):
             for iQt in range(1, 9):
-                print self.clos.GetBinContent(iY,iQt)
+                print self.closPlus.GetBinContent(iY,iQt)
+                print self.closMinus.GetBinContent(iY, iQt)
         fout = ROOT.TFile("accMap.root","recreate")
         fout.cd()
-        self.clos.Write()
+        self.closPlus.Write()
+        self.closMinus.Write()
         fout.Close()
-    def getHistos(self, chargeBin) :
+    def getHistos(self) :
         
         for iY in range(1, 7):
             for iQt in range(1, 9):
-               self.yields[(iY,iQt)]=0.
-        
+                self.yields["Wplus"][(iY,iQt)]=0.
+                self.yields["Wminus"][(iY, iQt)] = 0.
+
         basepath="templatesAC_Signal/"
-        #self.chargeBin = 2  if charge == 1 else 1
-        for sKind, sList in self.extSyst.iteritems():
-            self.histoDict[sKind]  = []
-            #print sKind, sList
-            for sname in sList[0]:#variations of each sKind
-                for htype in self.clist:
-                    for iQt in range(1,9):
-                        fpath = basepath + sKind + '/qt_{}_helXsecs_'.format(iQt) + htype + sname
-                        if sKind == 'Nominal' and sname == '': 
-                            fpath = basepath + sKind + '/qt_{}_helXsecs_'.format(iQt) + htype + sname
-                        print "Histo read:", fpath
-                        th3 = self.inFile.Get(fpath)
-                        if not th3: continue
-                        self.makeTH3slices(th3, sKind, chargeBin)
+        charges = ["Wplus","Wminus"]
+        for charge in charges:
+            for sKind, sList in self.extSyst.iteritems():
+                self.histoDict[charge][sKind]  = []
+                #print sKind, sList
+                for sname in sList[0]:#variations of each sKind
+                    for htype in self.clist:
+                        for iQt in range(1,9):
+                            fpath = basepath + sKind + '/'+charge+ '_qt_{}_helXsecs_'.format(iQt) + htype + sname
+                            print "Histo read:", fpath
+                            th3 = self.inFile.Get(fpath)
+                            if not th3: print colored('fpath not found', 'red')
+                            print colored(th3.Integral(), "yellow")
+                            self.makeTH3slices(th3, sKind)
         #self.uncorrelateEff()
         #self.symmetrisePDF()
         self.closureMap()
-        self.writeHistos(chargeBin)   
+        self.writeHistos()   
     def uncorrelateEff(self):
         aux = {}
         aux['WHSF'] = []
@@ -196,34 +215,32 @@ class plotter:
                 aux['LHEPdfWeight'].append(th2Down)
 
         self.histoDict.update(aux)
-    def writeHistos(self, chargeBin):
+    def writeHistos(self):
         print 'writing histograms'
-        foutName = 'Wplus_reco' if chargeBin == 2 else 'Wminus_reco'
-        foutName += '.root'
-        fout = ROOT.TFile.Open(self.outdir + '/' + foutName, "RECREATE")
-        fout.cd()
-        start = time()
-        for sKind, hlist in self.histoDict.iteritems():
-            #fout.mkdir(sKind)
-            #fout.cd(sKind)
+        charges = ["Wplus", "Wminus"]
+        for charge in charges:
+            foutName = charge+'_reco'
+            foutName += '.root'
+            fout = ROOT.TFile.Open(self.outdir + '/' + foutName, "RECREATE")
             fout.cd()
-            for h in hlist:
-                #th1=self.unroll2D(h)
-                h.Write()
-        fout.cd()
-        fout.Save()
-        fout.Close()
-        end = time()
-        print "time: {}".format(end - start)
+            for sKind, hlist in self.histoDict[charge].iteritems():
+                #fout.mkdir(sKind)
+                #fout.cd(sKind)
+                fout.cd()
+                for h in hlist:
+                    #th1=self.unroll2D(h)
+                    h.Write()
+            fout.cd()
+            fout.Save()
+            fout.Close()
+
 
 parser = argparse.ArgumentParser("")
 parser.add_argument('-o','--output', type=str, default='./',help="name of the output directory")
 parser.add_argument('-i','--input', type=str, default='./',help="name of the input directory root file")
-parser.add_argument('-AC','--AC', type=str, default='./',help="name of the input AC file root file")
+
 args = parser.parse_args()
 OUTPUT = args.output
 INPUT = args.input
-ACfile = args.AC
-p=plotter(outDir=OUTPUT, inDir = INPUT, ACfile=ACfile)
-#p.getHistos(1)
-p.getHistos(2)
+p=plotter(outDir=OUTPUT, inDir = INPUT)
+p.getHistos()
