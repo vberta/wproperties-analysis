8e05f15988eeed4c0d4d84a72f05215b91658d7c
Elisabetta Manca <elisabetta.manca@cern.ch>
2020-10-30 15:41:54 +0100

    testing fit regularisation

--- Fit/runFit.py
+++ Fit/runFit.py
@@ -7,17 +7,19 @@ import os
 charges = ["Wplus","Wminus"]
 for charge in charges:
     fmap = '/scratchnvme/emanca/wproperties-analysis/analysisOnGen/genInput_{}.root'.format(charge) 
-    f = fitUtils(fmap, channel=charge+"_reco", doSyst=True)
+    f = fitUtils(fmap, channel=charge+"_reco_reg", doSyst=True)
     f.fillProcessList()
     f.shapeFile()
     f.fillHelGroup()
     f.fillSumGroup()
     f.fillHelMetaGroup()
     f.makeDatacard()    
-    #assert(0)  
+    
     text2hd5f = 'text2hdf5.py --allowNegativeExpectation --maskedChan={}_xsec {}.pkl'.format(f.channel,f.channel)
     print 'executing', text2hd5f 
     os.system(text2hd5f)    
-    combinetf = 'combinetf.py --allowNegativePOI --binByBinStat --correlateXsecStat --doImpacts -t-1 {}.pkl.hdf5 -o fit_{}.root'.format(f.channel, f.channel)
+    combinetf = 'combinetf.py --allowNegativePOI --binByBinStat --correlateXsecStat --doRegularization --regularizationTau=1e1 -t-1 {}.pkl.hdf5 -o fit_{}.root'.format(
+        f.channel, f.channel)
     print 'executing', combinetf
     os.system(combinetf)
+    assert(0)
--- Fit/systToapply.py
+++ Fit/systToapply.py
@@ -26,37 +26,37 @@ systematicsDict = {
         "procs": ["Signal", "LowAcc"],
         "type": "shapeNoConstraint"
     },
-    "WHSFStat"  : {
-        "vars": ["WHSFSyst0Eta{}".format(i) for i in range(1, 49)]+["WHSFSyst1Eta{}".format(i) for i in range(1, 49)]+["WHSFSyst2Eta{}".format(i) for i in range(1, 49)],
-        "procs": ["Signal", "LowAcc"],
-        "type":"shape"},
-    "WHSFSyst":{
-        "vars": ["WHSFSystFlat"],
-        "procs": ["Signal","LowAcc"],
-        "type": "shape"},
-    "ptScale" : {
-        "vars": ["Eta{}zptsyst".format(j) for j in range(1, 5)] + ["Eta{}Ewksyst".format(j) for j in range(1, 5)] + ["Eta{}deltaMsyst".format(j) for j in range(1, 5)]+["Eta{}stateig{}".format(j, i) for i in range(0, 99) for j in range(1, 5)],
-        "procs": ["Signal"],
-        "type": "shape"
-    },
-    "jme" : {
-        "vars":["jesTotal", "unclustEn"],
-        "procs": ["Signal", "LowAcc"],
-        "type": "shape"
-    },
-    "PrefireWeight":{
-        "vars":["PrefireWeight"],
-        "procs": ["Signal", "LowAcc"],
-        "type": "shape"
-    },
-    "LHEPdfWeight" : {
-        "vars":["LHEPdfWeightHess{}".format(i+1) for i in range(60)],
-        "procs": ["Signal", "LowAcc"],
-        "type": "shape"
-    },
-    "alphaS" :{
-        "vars": ["alphaS"],
-        "procs": ["Signal", "LowAcc"],
-        "type": "shape"
-    }
+    #"WHSFStat"  : {
+    #    "vars": ["WHSFSyst0Eta{}".format(i) for i in range(1, 49)]+["WHSFSyst1Eta{}".format(i) for i in range(1, 49)]+["WHSFSyst2Eta{}".format(i) for i in range(1, 49)],
+    #    "procs": ["Signal", "LowAcc"],
+    #    "type":"shape"},
+    #"WHSFSyst":{
+    #    "vars": ["WHSFSystFlat"],
+    #    "procs": ["Signal","LowAcc"],
+    #    "type": "shape"},
+    #"ptScale" : {
+    #    "vars": ["Eta{}zptsyst".format(j) for j in range(1, 5)] + ["Eta{}Ewksyst".format(j) for j in range(1, 5)] + ["Eta{}deltaMsyst".format(j) for j in range(1, 5)]+["Eta{}stateig{}".format(j, i) for i in range(0, 99) for j in range(1, 5)],
+    #    "procs": ["Signal"],
+    #    "type": "shape"
+    #},
+    #"jme" : {
+    #    "vars":["jesTotal", "unclustEn"],
+    #    "procs": ["Signal", "LowAcc"],
+    #    "type": "shape"
+    #},
+    #"PrefireWeight":{
+    #    "vars":["PrefireWeight"],
+    #    "procs": ["Signal", "LowAcc"],
+    #    "type": "shape"
+    #},
+    #"LHEPdfWeight" : {
+    #    "vars":["LHEPdfWeightHess{}".format(i+1) for i in range(60)],
+    #    "procs": ["Signal", "LowAcc"],
+    #    "type": "shape"
+    #},
+    #"alphaS" :{
+    #    "vars": ["alphaS"],
+    #    "procs": ["Signal", "LowAcc"],
+    #    "type": "shape"
+    #}
 }
