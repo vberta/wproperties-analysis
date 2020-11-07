88cf0a3f24800b9cc3526e709cbf127695c4f2b6
Elisabetta Manca <elisabetta.manca@cern.ch>
2020-11-07 19:17:49 +0100

    all bkg inputs together in runAnalysisOnMC.py

--- analysisOnData/runAnalysisOnMC.py
+++ analysisOnData/runAnalysisOnMC.py
@@ -16,12 +16,13 @@ from getLumiWeight import getLumiWeight
 ROOT.gSystem.Load('bin/libAnalysisOnData.so')
 ROOT.gROOT.ProcessLine("gErrorIgnoreLevel = 2001;")
 
-def RDFprocessMC(fvec, outputDir, sample, xsec, fileSF, systType, pretendJob=True,SBana=False):
+def RDFprocess(fvec, outputDir, sample, xsec, fileSF, systType, pretendJob,SBana=False):
 
     p = RDFtree(outputDir = outputDir, inputFile = fvec, outputFile="{}_plots.root".format(sample), pretend=pretendJob)
-    #sample specific systematics
-    systematicsFinal=copy.deepcopy(systematics)
-    if systType != 2:
+    
+    if systType == 0: #this is data
+        p.branch(nodeToStart='input', nodeToEnd='defs', modules=[ROOT.baseDefinitions(False, False)])
+    elif systType < 2: #this is MC with no PDF variations
         p.branch(nodeToStart = 'input', nodeToEnd = 'defs', modules = [ROOT.baseDefinitions(True, False),ROOT.weightDefinitions(fileSF),getLumiWeight(xsec=xsec, inputFile=fvec)])
     else:
         p.branch(nodeToStart = 'input', nodeToEnd = 'defs', modules = [ROOT.baseDefinitions(True, False),ROOT.weightDefinitions(fileSF),getLumiWeight(xsec=xsec, inputFile=fvec),ROOT.Replica2Hessian()])
@@ -33,6 +34,8 @@ def RDFprocessMC(fvec, outputDir, sample, xsec, fileSF, systType, pretendJob=Tru
             weight = 'float(puWeight*PrefireWeight*lumiweight)'
         else:
             weight = 'float(puWeight*PrefireWeight*lumiweight*WHSF)'
+        if systType == 0:
+            weight = "float(1.)"
         
         #nominal part of the analysis
         nom = ROOT.vector('string')()
@@ -42,9 +45,11 @@ def RDFprocessMC(fvec, outputDir, sample, xsec, fileSF, systType, pretendJob=Tru
         if region == "Signal" or (region=='Sideband' and SBana):
             p.branch(nodeToStart = 'defs', nodeToEnd = 'prefit_{}/Nominal'.format(region), modules = [ROOT.muonHistos(cut, weight, nom,"Nom",0)])  
         p.branch(nodeToStart = 'defs', nodeToEnd = 'templates_{}/Nominal'.format(region), modules = [ROOT.templates(cut, weight, nom,"Nom",0)])    
-  
-       #weight variations
-        for s,variations in list(systematicsFinal.items()):
+        if systType == 0: #stop data here
+            continue
+        #sample specific systematics
+        #weight variations
+        for s,variations in list(systematics.items()):
             if "LHEScaleWeight" in s and systType != 2 :  continue
             if "LHEPdfWeight"   in s and systType != 2 :  continue
             if "alphaS"         in s and systType != 2 :  continue
@@ -92,15 +97,12 @@ def main():
         print("Running a test job over a few events")
     else:
         print("Running on full dataset")
-    ROOT.ROOT.EnableImplicitMT(32)
+    ROOT.ROOT.EnableImplicitMT(64)
     RDFtrees = {}
     samples={}
     with open('data/samples_2016.json') as f:
         samples = json.load(f)
     for sample in samples:
-        if not samples[sample]['datatype']=='MC': continue
-        #WJets to be run by a separate config
-        if 'WJets' in sample : continue
         print('analysing sample: %s'%sample)
         direc = samples[sample]['dir']
         xsec = samples[sample]['xsec']
@@ -120,15 +122,11 @@ def main():
         print(fvec) 
         fileSF = ROOT.TFile.Open("data/ScaleFactors_OnTheFly.root")
         systType = samples[sample]['systematics']
-        RDFtrees[sample] = RDFprocessMC(fvec, outputDir, sample, xsec, fileSF, systType, pretendJob, SBana)
+        RDFtrees[sample] = RDFprocess(fvec, outputDir, sample, xsec, fileSF, systType, pretendJob, SBana)
 
     #now trigger all the event loops at the same time:
     objList = []
     for sample in samples:
-        if not samples[sample]['datatype'] == 'MC':
-            continue
-        if 'WJets' in sample:
-            continue
         RDFtreeDict = RDFtrees[sample].getObjects()
         for node in RDFtreeDict:
             objList.extend(RDFtreeDict[node])
@@ -138,10 +136,6 @@ def main():
     #now write the histograms:
     
     for sample in samples:
-        if not samples[sample]['datatype'] == 'MC':
-            continue
-        if 'WJets' in sample:
-            continue
         RDFtrees[sample].getOutput()
     print('all samples processed in {} s'.format(time.time()-start))
 if __name__ == "__main__":
